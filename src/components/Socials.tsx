1133f4805a30dcead43b52af87b50a3ac43f300a
Marvin Chew <marvin@marvincyk.com>
2022-09-24 17:28:18 +1000

    Add copyright

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,4 +1,4 @@
-import { HStack, Icon, Link } from "@chakra-ui/react";
+import { HStack, Icon, Link, Text, VStack } from "@chakra-ui/react";
 import {
   FaGithubSquare,
   FaLinkedin,
@@ -9,20 +9,23 @@ import React from "react";
 
 const Socials = () => {
   return (
-    <HStack justify="center" spacing={10}>
-      <Link href="https://github.com/marvincyk" target="_blank">
-        <Icon as={FaGithubSquare} boxSize="40px" color="custom.200" />
-      </Link>
-      <Link href="https://www.linkedin.com/in/marvincyk/" target="_blank">
-        <Icon as={FaLinkedin} boxSize="40px" color="custom.200" />
-      </Link>
-      <Link href="https://www.facebook.com/marvincyk77" target="_blank">
-        <Icon as={FaFacebookSquare} boxSize="40px" color="custom.200" />
-      </Link>
-      <Link href="https://www.instagram.com/marvincyk/" target="_blank">
-        <Icon as={FaInstagramSquare} boxSize="40px" color="custom.200" />
-      </Link>
-    </HStack>
+    <VStack spacing={4}>
+      <HStack justify="center" spacing={10}>
+        <Link href="https://github.com/marvincyk" target="_blank">
+          <Icon as={FaGithubSquare} boxSize="40px" color="custom.200" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/marvincyk/" target="_blank">
+          <Icon as={FaLinkedin} boxSize="40px" color="custom.200" />
+        </Link>
+        <Link href="https://www.facebook.com/marvincyk77" target="_blank">
+          <Icon as={FaFacebookSquare} boxSize="40px" color="custom.200" />
+        </Link>
+        <Link href="https://www.instagram.com/marvincyk/" target="_blank">
+          <Icon as={FaInstagramSquare} boxSize="40px" color="custom.200" />
+        </Link>
+      </HStack>
+      <Text>{`©️ ${new Date().getFullYear()} Marvin Chew`}</Text>
+    </VStack>
   );
 };
 
