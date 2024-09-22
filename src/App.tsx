c199fd50c7d58aeaeac3cb216b2cd275b4afba8b
Marvin Chew <marvin@marvincyk.com>
2024-09-22 17:04:31 +1000

    Hide Projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import {
-	Box,
-	ChakraProvider,
-	Grid,
-	GridItem,
-	extendTheme,
+  Box,
+  ChakraProvider,
+  Grid,
+  GridItem,
+  extendTheme,
 } from "@chakra-ui/react";
 import "@fontsource/press-start-2p";
 import { Fade } from "react-awesome-reveal";
@@ -17,76 +17,76 @@ import Services from "./components/Services";
 import Socials from "./components/Socials";
 
 const theme = extendTheme({
-	fonts: {
-		heading: `"Press Start 2P", cursive`,
-		body: `"Press Start 2P", cursive`,
-	},
-	colors: {
-		custom: {
-			100: "#D8E9A8",
-			200: "#32CD32",
-			300: "#1E5128",
-			400: "#191A19",
-		},
-	},
+  fonts: {
+    heading: `"Press Start 2P", cursive`,
+    body: `"Press Start 2P", cursive`,
+  },
+  colors: {
+    custom: {
+      100: "#D8E9A8",
+      200: "#32CD32",
+      300: "#1E5128",
+      400: "#191A19",
+    },
+  },
 });
 
 export const App = () => (
-	<ChakraProvider theme={theme}>
-		<Box bgColor="black" textColor="custom.200">
-			<Grid
-				minH="100vh"
-				templateColumns="repeat(12, 1fr)"
-				columnGap={6}
-				rowGap={{ base: "60", lg: "80" }}
-				p={8}
-			>
-				<GridItem
-					colSpan={12}
-					display="flex"
-					flexDirection="column"
-					gap={{ base: 20, md: 40, lg: 60 }}
-				>
-					<Navbar />
-					<Fade>
-						<Hero />
-					</Fade>
-				</GridItem>
-				<GridItem colSpan={12} id="services">
-					<Fade>
-						<Services />
-					</Fade>
-				</GridItem>
-				<GridItem colSpan={12} id="education">
-					<Fade>
-						<Education />
-					</Fade>
-				</GridItem>
-				<GridItem colSpan={12} id="experience">
-					<Fade>
-						<Experience />
-					</Fade>
-				</GridItem>
-				<GridItem colSpan={12} id="projects">
+  <ChakraProvider theme={theme}>
+    <Box bgColor="black" textColor="custom.200">
+      <Grid
+        minH="100vh"
+        templateColumns="repeat(12, 1fr)"
+        columnGap={6}
+        rowGap={{ base: "60", lg: "80" }}
+        p={8}
+      >
+        <GridItem
+          colSpan={12}
+          display="flex"
+          flexDirection="column"
+          gap={{ base: 20, md: 40, lg: 60 }}
+        >
+          <Navbar />
+          <Fade>
+            <Hero />
+          </Fade>
+        </GridItem>
+        <GridItem colSpan={12} id="services">
+          <Fade>
+            <Services />
+          </Fade>
+        </GridItem>
+        <GridItem colSpan={12} id="education">
+          <Fade>
+            <Education />
+          </Fade>
+        </GridItem>
+        <GridItem colSpan={12} id="experience">
+          <Fade>
+            <Experience />
+          </Fade>
+        </GridItem>
+        {/* <GridItem colSpan={12} id="projects">
 					<Fade>
 						<Projects />
 					</Fade>
-				</GridItem>
-				<GridItem colSpan={12} id="socials">
-					<Fade>
-						<Socials />
-					</Fade>
-				</GridItem>
-			</Grid>
-		</Box>
-		<ScrollToTop
-			smooth
-			style={{
-				justifyContent: "center",
-				alignItems: "center",
-				display: "flex",
-				backgroundColor: "#4E9F3D",
-			}}
-		/>
-	</ChakraProvider>
+				</GridItem> */}
+        <GridItem colSpan={12} id="socials">
+          <Fade>
+            <Socials />
+          </Fade>
+        </GridItem>
+      </Grid>
+    </Box>
+    <ScrollToTop
+      smooth
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        backgroundColor: "#4E9F3D",
+      }}
+    />
+  </ChakraProvider>
 );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,105 +1,105 @@
 import {
-	Box,
-	HStack,
-	Image,
-	List,
-	ListItem,
-	useDisclosure,
-	IconButton,
-	Drawer,
-	DrawerOverlay,
-	DrawerContent,
-	DrawerBody,
-	DrawerCloseButton,
+  Box,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  useDisclosure,
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerBody,
+  DrawerCloseButton,
 } from "@chakra-ui/react";
 import { Link } from "react-scroll";
 import React from "react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-	return (
-		<Box as="nav">
-			<HStack justify="space-between" align="center">
-				<Image src="/logo.svg" w="300px" h="60px" ml={-16} />
-				<IconButton
-					aria-label="Toggle navigation menu"
-					icon={<HamburgerIcon w={10} h={10} />}
-					variant="unstyled"
-					colorScheme="green"
-					display={{ lg: "none" }}
-					onClick={onOpen}
-				/>
-				<Drawer placement="right" onClose={onClose} isOpen={isOpen}>
-					<DrawerOverlay />
-					<DrawerContent>
-						<DrawerCloseButton color="custom.200" size="lg" />
-						<DrawerBody bgColor="black" textColor="custom.200">
-							<List
-								display="flex"
-								flexDirection="column"
-								gap={20}
-								alignItems="center"
-								mt={20}
-							>
-								<NavItems onClose={onClose} />
-							</List>
-						</DrawerBody>
-					</DrawerContent>
-				</Drawer>
-				<List display={{ base: "none", lg: "flex" }} gap={20}>
-					<NavItems onClose={onClose} />
-				</List>
-			</HStack>
-		</Box>
-	);
+  return (
+    <Box as="nav">
+      <HStack justify="space-between" align="center">
+        <Image src="/logo.svg" w="300px" h="60px" ml={-16} />
+        <IconButton
+          aria-label="Toggle navigation menu"
+          icon={<HamburgerIcon w={10} h={10} />}
+          variant="unstyled"
+          colorScheme="green"
+          display={{ lg: "none" }}
+          onClick={onOpen}
+        />
+        <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton color="custom.200" size="lg" />
+            <DrawerBody bgColor="black" textColor="custom.200">
+              <List
+                display="flex"
+                flexDirection="column"
+                gap={20}
+                alignItems="center"
+                mt={20}
+              >
+                <NavItems onClose={onClose} />
+              </List>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+        <List display={{ base: "none", lg: "flex" }} gap={20}>
+          <NavItems onClose={onClose} />
+        </List>
+      </HStack>
+    </Box>
+  );
 };
 
 interface NavItemsProps {
-	onClose: () => void;
+  onClose: () => void;
 }
 
 const NavItems = ({ onClose }: NavItemsProps) => {
-	return (
-		<>
-			<Link to="services" smooth>
-				<ListItem
-					_hover={{
-						cursor: "pointer",
-						color: "white",
-					}}
-					transition="color 200ms"
-					onClick={onClose}
-				>
-					services
-				</ListItem>
-			</Link>
-			<Link to="education" smooth>
-				<ListItem
-					_hover={{
-						cursor: "pointer",
-						color: "white",
-					}}
-					transition="color 200ms"
-					onClick={onClose}
-				>
-					education
-				</ListItem>
-			</Link>
-			<Link to="experience" smooth>
-				<ListItem
-					_hover={{
-						cursor: "pointer",
-						color: "white",
-					}}
-					transition="color 200ms"
-					onClick={onClose}
-				>
-					experience
-				</ListItem>
-			</Link>
-			<Link to="projects" smooth>
+  return (
+    <>
+      <Link to="services" smooth>
+        <ListItem
+          _hover={{
+            cursor: "pointer",
+            color: "white",
+          }}
+          transition="color 200ms"
+          onClick={onClose}
+        >
+          services
+        </ListItem>
+      </Link>
+      <Link to="education" smooth>
+        <ListItem
+          _hover={{
+            cursor: "pointer",
+            color: "white",
+          }}
+          transition="color 200ms"
+          onClick={onClose}
+        >
+          education
+        </ListItem>
+      </Link>
+      <Link to="experience" smooth>
+        <ListItem
+          _hover={{
+            cursor: "pointer",
+            color: "white",
+          }}
+          transition="color 200ms"
+          onClick={onClose}
+        >
+          experience
+        </ListItem>
+      </Link>
+      {/* <Link to="projects" smooth>
 				<ListItem
 					_hover={{
 						cursor: "pointer",
@@ -110,9 +110,9 @@ const NavItems = ({ onClose }: NavItemsProps) => {
 				>
 					projects
 				</ListItem>
-			</Link>
-		</>
-	);
+			</Link> */}
+    </>
+  );
 };
 
 export default Navbar;
